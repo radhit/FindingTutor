8e937a18e7491e12d9586cb0636a0e1a770f8991
Riska Adhita <risk.adhita@gmail.com>
2017-03-14 15:24:13 +0700

    [cek] dapat jarak dan marker murid

--- app/src/main/java/com/manajemeninformasi/riska/findingtutor/DetilMuridActivity.java
+++ app/src/main/java/com/manajemeninformasi/riska/findingtutor/DetilMuridActivity.java
@@ -1,7 +1,11 @@
 package com.manajemeninformasi.riska.findingtutor;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,11 +14,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.manajemeninformasi.riska.findingtutor.setting.Database;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DetilMuridActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private TextView nama, kelas, pelajaran, alamat, tanggal, hari, jam, biaya;
+    private TextView nama, kelas, pelajaran, alamat, tanggal, hari, jam, biaya, jarak;
+    private Bundle bundle;
+    private Database db;
+    private String alamatTutordb;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +36,10 @@ public class DetilMuridActivity extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Bundle bundle = getIntent().getBundleExtra("bundle");
+        db = new Database(this);
+        alamatTutordb = db.getAlamatuser();
+        Log.d("alamat tutor : ", alamatTutordb);
+        bundle = getIntent().getBundleExtra("bundle");
 
         nama = (TextView) findViewById(R.id.tvnama);
         kelas = (TextView) findViewById(R.id.tvkelas);
@@ -34,6 +49,7 @@ public class DetilMuridActivity extends FragmentActivity implements OnMapReadyCa
         hari = (TextView) findViewById(R.id.tvhari);
         jam = (TextView) findViewById(R.id.tvjam);
         biaya = (TextView) findViewById(R.id.tvbiaya);
+        jarak = (TextView) findViewById(R.id.tvjarak);
 
         nama.setText(bundle.getString("nama"));
         kelas.setText(bundle.getString("kelas"));
@@ -44,6 +60,19 @@ public class DetilMuridActivity extends FragmentActivity implements OnMapReadyCa
         jam.setText(bundle.getString("jam"));
         biaya.setText(bundle.getString("biaya"));
 
+    }
+
+    public void jarak(double latMurid, double longMurid, double latTutor, double longTutor)
+    {
+        Location murid = new Location("murid");
+        Location tutor = new Location("tutor");
+        murid.setLatitude(latMurid);
+        murid.setLongitude(longMurid);
+        tutor.setLatitude(latTutor);
+        tutor.setLongitude(longTutor);
+
+        float getJarak = tutor.distanceTo(murid)/1000;
+        jarak.setText(String.valueOf(getJarak)+" Km");
     }
 
 
@@ -59,10 +88,32 @@ public class DetilMuridActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        markerMurid();
+    }
+    public void markerMurid()
+    {
+        Log.d("alamat : ", alamat.getText().toString());
+        geocoder = new Geocoder(getBaseContext());
+        try {
+            List<Address> listMurid = geocoder.getFromLocationName(alamat.getText().toString(),1);
+            Address alamatMurid = listMurid.get(0);
+            Double latMurid = alamatMurid.getLatitude();
+            Double longMurid = alamatMurid.getLongitude();
+            LatLng latlongMurid = new LatLng(latMurid, longMurid);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            List<Address> listTutor = geocoder.getFromLocationName(alamatTutordb,1);
+            Address alamatTutor = listTutor.get(0);
+            Double latTutor = alamatTutor.getLatitude();
+            Double longTutor = alamatTutor.getLongitude();
+
+
+            mMap.addMarker(new MarkerOptions().position(latlongMurid).title(alamat.getText().toString()));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlongMurid));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+            jarak(latMurid, longMurid, latTutor, longTutor);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/manajemeninformasi/riska/findingtutor/setting/Connect.java
+++ app/src/main/java/com/manajemeninformasi/riska/findingtutor/setting/Connect.java
@@ -5,7 +5,7 @@ package com.manajemeninformasi.riska.findingtutor.setting;
  */
 
 public class Connect {
-    public static final String ROOT_URL = "http://192.168.1.100/findingtutor/";
+    public static final String ROOT_URL = "http://10.151.44.224/findingtutor/";
     public static final String REGISTER_URL = ROOT_URL+"register_controller";
     public static final String LOGIN_URL = ROOT_URL+"login_controller";
     public static final String TAMBAHKEAHLIAN_URL = ROOT_URL+"tambahkeahlian_controller";
